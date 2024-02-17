a1238b455216b8cc02c77154a3b3cbac14995f28
SunrisePaladin <Revaloise@yandex.ru>
2024-02-17 20:48:35 +0300

    Jawa

---
+++ Java/my_projects/program.java
@@ -0,0 +1,13 @@
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        System.out.println("aboba.jpg"); //aboba.jpg
+        int [] arr = new int[] {1,2,3,4,5,6,7,8};
+        arr[0] = -1;
+        int arr_c[] = {1,2,3,4,5,6,7};
+    }
+}
+
