27724b1a948656fe8508aec7d8e3623b4883f656
SunrisePaladin <revaloise@yandex.ru>
2024-04-14 22:50:28 +0300

    something

--- Питон/Progress task/App.py
+++ Питон/Progress task/App.py
@@ -1,29 +1,36 @@
+# from __future__ import unicode_literals
 import json
 import dateutil.parser as dparser
 from datetime import *
 
 # Передайте номер для чтения заметки по номеру
 def readNote(number=date.today()):
-    ifstream = open("notes.txt", 'r', "UTF-8")
+    ifstream = open("notes.json", 'r')
     if isinstance(number, int):
         for i in range(number):
+            # дата, заголовок, содержимое
+            ifstream.readline()
+            ifstream.readline()
             ifstream.readline()
         resline = ifstream.readline()
         print(resline)
     else:
         #реализовать фильтрацию по дате
         pass
+    ifstream.close()
 
 def writeNote():
-    ofstream = open("notes.txt", 'a+', "UTF-8")
+    ofstream = open("./Progress task/notes.txt", 'a')
     line = input("Введите заметку: ")
     if line != "":
-        ofstream.write(line)
+        ofstream.write("\n" + line)
+        print("написал")
     else:
         print("Ошибочный ввод!")
+        ofstream.close()
 
 def readNotes():
-    ifstream = open("notes.txt", 'r', "UTF-8")
+    ifstream = open("notes.json", 'r')
     for line in ifstream.readlines():
         print(line)
 
@@ -34,12 +41,11 @@ print("================================")
 
 while True:
     print("Команды:\n1. Написать заметку\n2. Прочитать заметку по номеру/дате/все заметки\n3. Редактировать заметку\n4. Удалить заметку")
-    com = int(input("Введите номер команды"))
+    com = int(input("Введите номер команды: "))
     match com:
         case 1:
             writeNote()
-            break;
-        case range(2):
+        case 2:
             res = input("Все? [Y|N]")
             if res.lower() == "y": readNotes()
             else:
@@ -52,4 +58,7 @@ while True:
                     dt = dparser.parse(msgdate)
                     if dt is not None:
                         readNote(dt)
+        case _:
+            pass
+        
 
